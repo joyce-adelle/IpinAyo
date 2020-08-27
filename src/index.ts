284e336f6e92525917a6490049576995bcbee9aa
joyce-adelle <jadelusi11@gmail.com>
2020-08-27 17:53:19 +0100

    initial setup

--- src/index.ts
+++ src/index.ts
@@ -1,21 +1,23 @@
 import "reflect-metadata";
-import {createConnection} from "typeorm";
-import {User} from "./entity/User";
+import { createConnection, Connection} from "typeorm";
+import { ApolloServer } from "apollo-server";
+import { buildSchema } from  "type-graphql";
 
-createConnection().then(async connection => {
 
-    console.log("Inserting a new user into the database...");
-    const user = new User();
-    user.firstName = "Timber";
-    user.lastName = "Saw";
-    user.age = 25;
-    await connection.manager.save(user);
-    console.log("Saved a new user with id: " + user.id);
+var connection: Connection;
 
-    console.log("Loading users from the database...");
-    const users = await connection.manager.find(User);
-    console.log("Loaded users: ", users);
+async function main() {
+  connection = await createConnection();
+  const schema = await buildSchema({
+    resolvers: [""] 
+  });
+  const server = new ApolloServer({ schema });
+  await server.listen(4000);
+  console.log("Server has started!");
+}
 
-    console.log("Here you can setup and run express/koa/any other framework.");
+export function connected() { 
+    return typeof connection !== 'undefined'; 
+}
 
-}).catch(error => console.log(error));
+main();

60c0f77e8d40997d0668d2c922c97c65f9fe3d7f
joyce-adelle <jadelusi11@gmail.com>
2020-09-03 19:06:02 +0100

    created category repository

--- src/db/repositories/CategoryRepository.ts
+++ src/db/repositories/CategoryRepository.ts
@@ -1,4 +1,4 @@
-import { EntityRepository, Repository, Like, TreeRepository } from "typeorm";
+import { EntityRepository, TreeRepository, getConnection } from "typeorm";
 import * as util from "util";
 import { Category } from "../entities/Category";
 import { CreateCategory } from "../inputInterfaces/CreateCategory";
@@ -7,15 +7,22 @@ import { UpdateCategory } from "../inputInterfaces/UpdateCategory";
 @EntityRepository(Category)
 export class CategoryRepository extends TreeRepository<Category> {
   async createAndSave(category: CreateCategory): Promise<Category> {
-    let categoryDet = new Category();
-    Object.assign(categoryDet, category);
-    let newCategory = await this.save(categoryDet);
-    return newCategory;
+    try {
+      let categoryDet = new Category();
+      categoryDet.name = category.name;
+      if (category.parentId) {
+        categoryDet.parent = await this.findCategoryById(category.parentId);
+      }
+      let newCategory = await this.save(categoryDet);
+      return newCategory;
+    } catch (error) {
+      throw new Error(error.message);
+    }
   }
 
   async allCategories(): Promise<Category[]> {
-    let category = await this.findTrees();
-    return category;
+    let categories = await this.findTrees();
+    return categories;
   }
 
   async findCategoryById(id: string): Promise<Category> {
@@ -23,38 +30,60 @@ export class CategoryRepository extends TreeRepository<Category> {
 
     if (!CategoryRepository.isCategory(obj)) {
       throw new Error(
-        `Category' id ${util.inspect(id)} did not retrieve a RelatedPhrase`
+        `Category' id ${util.inspect(id)} did not retrieve a Category`
       );
     }
     return obj;
   }
 
-  //   async findRelatedMusicIdsByPhrase(phrase: string): Promise<string[]> {
-  //     let relatedRels: Category[] = [];
-  //     let rel = await this.find({
-  //       where: { phrase: Like(`%${phrase.trim()}%`) },
-  //     });
-  //     for (let key of rel) {
-  //       relatedRels.push(
-  //         await this.findOne({
-  //           where: { groupId: key.groupId },
-  //         })
-  //       );
-  //     }
-  //     let musicIds: string[] = [];
-  //     for (let key of relatedRels) {
-  //       musicIds.push(...key.relatedMusicIds);
-  //     }
-  //     return [...new Set(musicIds)];
-  //   }
+  async findRelatedMusicIdsByCategoryId(categoryId: string): Promise<string[]> {
+    try {
+      let rel = await this.findCategoryById(categoryId);
+      let relatedRels = await this.findDescendants(rel);
 
+      let musicIds: string[] = [];
+      for (let key of relatedRels) {
+        musicIds.push(...key.relatedMusicIds);
+      }
+      return [...new Set(musicIds)];
+    } catch (error) {
+      throw new Error(error.message);
+    }
+  }
+
+  //not a complete solution
   async updateCategory(
     id: string,
     category: UpdateCategory
   ): Promise<Category> {
     try {
       let categoryDet = await this.findCategoryById(id);
-      Object.assign(categoryDet, category);
+      if (category.name) {
+        categoryDet.name = category.name;
+      }
+      if (category.parentId) {
+        if (category.parentId.toLowerCase() === "null") {
+          categoryDet.parent = null;
+        } else {
+          categoryDet.parent = await this.findCategoryById(category.parentId);
+        }
+
+        await getConnection().query(
+          `DELETE a FROM category_closure AS a
+          JOIN category_closure AS d ON a.id_descendant = d.id_descendant
+          LEFT JOIN category_closure AS x
+          ON x.id_ancestor = d.id_ancestor AND x.id_descendant = a.id_ancestor
+          WHERE d.id_ancestor = ${id} AND x.id_ancestor IS NULL`
+        );
+
+        await getConnection().query(
+          `INSERT INTO category_closure (id_ancestor, id_descendant)
+          SELECT supertree.id_ancestor, subtree.id_descendant
+          FROM category_closure AS supertree JOIN category_closure AS subtree
+          WHERE subtree.id_ancestor = ${id}
+          AND supertree.id_descendant = ${category.parentId}`
+        );
+      }
       let updatedCategory = await this.save(categoryDet);
       return updatedCategory;
     } catch (error) {
@@ -63,9 +92,6 @@ export class CategoryRepository extends TreeRepository<Category> {
   }
 
   static isCategory(category: any): category is Category {
-    return (
-      typeof category === "object" &&
-      typeof category.name === "string" 
-    );
+    return typeof category === "object" && typeof category.name === "string";
   }
 }
--- src/index.ts
+++ src/index.ts
@@ -2,21 +2,21 @@ import "reflect-metadata";
 import { createConnection, Connection } from "typeorm";
 import { ApolloServer } from "apollo-server";
 import { buildSchema } from "type-graphql";
-import { RelatedPhrasesRepository } from "./db/repositories/RelatedPhrasesRepository";
+import { CategoryRepository } from "./db/repositories/CategoryRepository";
 import { CompositionType } from "./utilities/CompositionType";
 
 var connection: Connection;
 
 async function main() {
   connection = await createConnection();
-  let rep = connection.getCustomRepository(RelatedPhrasesRepository);
+  let rep = connection.getCustomRepository(CategoryRepository);
   // const schema = await buildSchema({
   //   resolvers: [""],
   // });
   // const server = new ApolloServer({ schema });
   // await server.listen(4000);
   console.log("Server has started!");
-  let c = await rep.findRelatedMusicIdsByPhrase(" holy ")
+  let c = await rep.findRelatedMusicIdsByCategoryId('4')
   console.log(c);
 }
 
